18d28219a33859f72e3e611500113b10164e616c
mehdisnrt-arch <mehdisnrt@gmail.com>
2026-08-06 13:11:14 +0100

    Load startup event binding fix

--- gallery-fix.js
+++ gallery-fix.js
@@ -36,6 +36,7 @@
     document.write('<script src="energy-manual-remove-v13.js"><\/script>');
     document.write('<script src="backend-version-v14.js"><\/script>');
     document.write('<script src="report-title-v15.js"><\/script>');
+    document.write('<script src="wire-events-fix-v17.js"><\/script>');
     return;
   }
 
@@ -52,7 +53,9 @@
         loadScript('energy-empty-v11.js', () => {
           loadScript('date-sync-v12.js', () => {
             loadScript('energy-manual-remove-v13.js', () => {
-              loadScript('backend-version-v14.js', () => loadScript('report-title-v15.js'));
+              loadScript('backend-version-v14.js', () => {
+                loadScript('report-title-v15.js', () => loadScript('wire-events-fix-v17.js'));
+              });
             });
           });
         });
